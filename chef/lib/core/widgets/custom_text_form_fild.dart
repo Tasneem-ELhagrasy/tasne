85c6c84b534696fed11b33e17c5222cc12444744
Tasneem-ELhagrasy <tasneemahmed507@gmail.com>
2023-09-05 18:59:48 +0300

    comit & push

---
+++ chef/lib/core/widgets/custom_text_form_fild.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../uitls/app_colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField(
+      {super.key,
+      required this.controller,
+      this.hint,
+      this.lable,
+      this.validate,
+      this.isPassword = false,
+      this.icon,
+      this.suffixiIconOnPressed});
+  final TextEditingController controller;
+  final String? hint;
+  final String? lable;
+  final String? Function(String?)? validate;
+  final bool isPassword;
+  final IconData? icon;
+  final VoidCallback? suffixiIconOnPressed;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: TextEditingController(),
+      cursorColor: AppColors.orange,
+      obscureText: isPassword,
+      validator: validate,
+      decoration: InputDecoration(
+        hintText: hint,
+        labelText: lable,
+        suffixIcon: IconButton(
+          onPressed: suffixiIconOnPressed,
+          icon: Icon(
+            icon,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- chef/lib/features/auth/presentation/cubit/componanents/screens/login_screen.dart
+++ chef/lib/features/auth/presentation/cubit/componanents/screens/login_screen.dart
@@ -3,7 +3,11 @@ import 'package:chef/core/uitls/app_assets.dart';
 
 import 'package:chef/core/uitls/app_string.dart';
 import 'package:chef/core/widgets/custom_image.dart';
+import 'package:chef/features/auth/presentation/cubit/componanents/screens/chang_longe.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../../core/widgets/custom_text_form_fild.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -12,21 +16,64 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                const CustomImage(
-                  imgPath: AppAssets.bg2,
-                  w: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  const CustomImage(
+                    imgPath: AppAssets.bg2,
+                    w: double.infinity,
+                  ),
+                  Center(
+                      child: Text(AppString.welcomeBack.tr(context),
+                          style: Theme.of(context).textTheme.displayLarge)),
+                ],
+              ),
+              SizedBox(
+                height: 100.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      controller: TextEditingController(),
+                      hint: AppString.email.tr(context),
+                    ),
+                    SizedBox(height: 32.h),
+                    CustomTextFormField(
+                      controller: TextEditingController(),
+                      hint: AppString.password.tr(context),
+                      isPassword: true,
+                      icon: Icons.remove_red_eye,
+                      suffixiIconOnPressed: () {},
+                      validate: (data) {
+                        if (data!.length < 6 || data.isEmpty) {
+                          return AppString.pleaseEnterValidPassword.tr(context);
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 24.h),
+                    Row(
+                      children: [
+                        Text(
+                          AppString.forgetPassword.tr(context),
+                        ),
+                        SizedBox(height: 32.h),
+                        CustomButton(
+                          onPressed: () {},
+                          text: AppString.signIn.tr(context),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                Center(
-                    child: Text(AppString.welcomeBack.tr(context),
-                        style: Theme.of(context).textTheme.displayLarge)),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
